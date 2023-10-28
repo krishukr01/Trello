79a92336f88b7a1ebb28395a7fda32771abd3523
Krishan Kumar Safi <142893219+krishukr01@users.noreply.github.com>
2023-10-28 13:01:51 +0530

    Navbar : added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,28 @@
+import { PiStarThin } from "react-icons/pi";
+import { SlPeople } from "react-icons/sl";
+import { BiCalendar } from "react-icons/bi";
 export const Navbar = () => {
-  return <nav className="h-16 bg-secondary">Krishan</nav>;
+  return (
+    <nav className="h-16 bg-secondary flex justify-between items-center text-primary px-16">
+      <section className=" text-xl flex items-center gap-5 cursor-pointer">
+        <text>Project Management</text>
+        <text>
+          <PiStarThin />
+        </text>
+        <text>
+          <SlPeople />
+        </text>
+      </section>
+      <section className="flex items-center gap-5 cursor-pointer">
+        <text>
+          <BiCalendar />
+        </text>
+        <div className="avatar placeholder">
+          <div className="bg-neutral-focus text-neutral-content rounded-full w-10">
+            <span>MX</span>
+          </div>
+        </div>
+      </section>
+    </nav>
+  );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,11 @@ export default {
           "linear-gradient(140deg, hsl(246deg 68% 28%) 0%, hsl(261deg 68% 28%) 20%, hsl(272deg 68% 28%) 35%, hsl(281deg 66% 28%) 47%, hsl(290deg 63% 28%) 57%, hsl(298deg 60% 28%) 66%, hsl(306deg 57% 31%) 74%, hsl(313deg 55% 33%) 82%, hsl(319deg 53% 36%) 90%, hsl(324deg 50% 39%) 100%)",
       },
       colors: {
-        primary: "#7C2D70",
+        primary: "#FFFFFF",
         secondary: "#0000003d",
         gradient: "#201778",
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
