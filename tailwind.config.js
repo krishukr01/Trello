f825da3a0225e3b64b137b58d309a3bad402bbd4
Krishan Kumar Safi <142893219+krishukr01@users.noreply.github.com>
2023-10-28 11:52:51 +0530

    Tailwdin extend theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     extend: {
       backgroundImage: {
         "custom-gradient":
-          "linear-gradient(140deg, hsl(246deg 68% 28%) 0%, hsl(261deg 68% 28%) 11%, hsl(272deg 68% 28%) 22%, hsl(281deg 66% 28%) 33%, hsl(290deg 63% 28%) 44%, hsl(298deg 60% 28%) 56%, hsl(306deg 57% 31%) 67%, hsl(313deg 55% 33%) 78%, hsl(319deg 53% 36%) 89%, hsl(324deg 50% 39%) 100%)",
+          "linear-gradient(140deg, hsl(246deg 68% 28%) 0%, hsl(261deg 68% 28%) 20%, hsl(272deg 68% 28%) 35%, hsl(281deg 66% 28%) 47%, hsl(290deg 63% 28%) 57%, hsl(298deg 60% 28%) 66%, hsl(306deg 57% 31%) 74%, hsl(313deg 55% 33%) 82%, hsl(319deg 53% 36%) 90%, hsl(324deg 50% 39%) 100%)",
       },
       colors: {
         primary: "#7C2D70",
